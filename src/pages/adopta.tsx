e5d371df4bce51cf87b3b76afbd8d134958fcc7d
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2025-03-15 18:59:54 +0200

    recent

---
+++ src/pages/adopta.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function AdoptaPage() {
+    return (
+        <div>
+            <Header />
+            {/* Continua a aici */}
+            <Footer />
+        </div>
+    );
+}
