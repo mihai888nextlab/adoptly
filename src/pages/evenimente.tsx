d8799768f82629b253d3e47a54db64a814a38a6c
MoloAlbert <albertmolocea@gmail.com>
2025-03-15 22:12:24 +0200

    doneaza page
    am facut ceva da e rau, nu e gata inca

--- src/pages/evenimente.tsx
+++ src/pages/evenimente.tsx
@@ -1,12 +1,12 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 
-export default function Evenimente(){
-    return (
-        <main>
-            <Header />
-            <h1>Evenimente</h1>
-            <Footer />
-        </main>
-    );
+export default function Evenimente() {
+  return (
+    <main>
+      <Header />
+      <h1>Evenimente</h1>
+      <Footer />
+    </main>
+  );
 }
