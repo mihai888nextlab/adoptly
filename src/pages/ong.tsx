3432588937132fee07b09548efbf429291a98291
ramiDev <147836937+mihai888nextlab@users.noreply.github.com>
2025-03-15 16:07:28 +0200

    made ong page

--- src/pages/ong.tsx
+++ src/pages/ong.tsx
@@ -3,83 +3,109 @@ import Footer from "@/components/footer";
 import { useEffect, useState } from "react";
 import { Playwrite_HU, Poppins } from "next/font/google";
 import FancyButton from "@/components/fancyButton";
-import  Link  from "next/link";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { Url } from "next/dist/shared/lib/router/router";
+import { User } from "@/lib/models/user";
+
 interface Shelter {
-    name: string;
-    description: string;
-    website: string;
+  _id: string; // Ensure _id is of type string
+  name: string;
+  description: string;
+  website: string;
 }
 
 export default function ONG() {
-    const [ongs, setOngs] = useState<Shelter[]>([]);
-   
+  const [ongs, setOngs] = useState<Shelter[]>([]);
 
-    useEffect(() => {
-        fetch("/api/getShelters")
-            .then(response => response.json())
-            .then(data => { setOngs(data)
-                console.log(data);
-            })
-            .catch(error => console.error("Error fetching NGOs:", error));
-    }, []);
-    return (
-        <div className={` smin-h-screen flex flex-col place-self-center items-center`}>
-            <Header />
-            <main className="flex-grow  justify-center items-center bg-center px-4 py-8">
+  useEffect(() => {
+    fetch("/api/getShelters")
+      .then((response) => response.json())
+      .then((data) => {
+        setOngs(data);
+        console.log(data);
+      })
+      .catch((error) => console.error("Error fetching NGOs:", error));
+  }, []);
 
-                <h1 className="text-3xl font-bold text-center mb-6">Partenerii Noștri - ONG-uri</h1>
-                <p className="text-lg text-center mb-8">
-                    Susținem numeroase organizații non-guvernamentale care contribuie la o lume mai bună. Descoperă mai jos partenerii noștri.
-                </p>
-                <div className="grid grid-cols-1 text-center justify-center align-midle md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {ongs.length > 0 ? (
-                        ongs.map((ong, index) => (
-                            <Link key={index} href={`/ong/${ong.name}`} passHref>
-                                <div className="flex justify-center mb-8 cursor-pointer">
-                                    <div className="w-full max-w-xs bg-gradient-to-r from-blue-500 via-purple-600 to-indigo-700 border border-transparent rounded-lg shadow-xl transform transition-transform hover:scale-105 hover:shadow-2xl p-8">
-                                        <h2 className="text-3xl font-extrabold text-center text-white mb-6">{ong.name}</h2>
-                                        <p className="text-center text-white opacity-80 mb-6 text-lg">
-                                            {ong.description}
-                                        </p>
-                                        {ong.website && (
-                                            <a
-                                                href={ong.website}
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                                className="block text-center text-white bg-blue-700 hover:bg-blue-800 rounded-full py-2 px-6 mt-4 transition-all duration-300 ease-in-out"
-                                            >
-                                                Vizitează site-ul
-                                            </a>
-                                        )}
-                                    </div>
-                                </div>
-    </Link>
-))
-                    ) : (
-                <p className="text-center text-gray-500 justify-center flex items-center justify-center text-xl font-semibold">
-                <span className="mr-2">Încărcăm datele...</span>
-                <svg className="animate-spin h-5 w-5 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                    <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none"/>
-                    <path d="M4 12a8 8 0 1 1 16 0A8 8 0 0 1 4 12z" fill="none" stroke="currentColor" strokeWidth="4"/>
-                </svg>
-                </p>
-                    )}
-                </div>
-                <div className="text-center">
-                    <p className="text-lg text-center mb-8">
-                        Ești un ONG care dorește să devi parte din echipa de parteneri alătură-te.
+  return (
+    <div
+      className={`smin-h-screen flex flex-col place-self-center items-center`}
+    >
+      <Header />
+      <main className="flex-grow justify-center items-center bg-center px-4 py-8">
+        <h1 className="text-3xl font-bold text-center mb-6">
+          Partenerii Noștri - ONG-uri
+        </h1>
+        <p className="text-lg text-center mb-8">
+          Susținem numeroase organizații non-guvernamentale care contribuie la o
+          lume mai bună. Descoperă mai jos partenerii noștri.
+        </p>
+        <div className="grid grid-cols-1 text-center justify-center align-midle md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {ongs.length > 0 ? (
+            ongs.map((ong, index) => (
+              <Link key={index} href={`/ong/${ong._id}`} passHref>
+                <div className="flex justify-center mb-8 cursor-pointer">
+                  <div className="w-full max-w-xs bg-gradient-to-r from-blue-500 via-purple-600 to-indigo-700 border border-transparent rounded-lg shadow-xl transform transition-transform hover:scale-105 hover:shadow-2xl p-8">
+                    <h2 className="text-3xl font-extrabold text-center text-white mb-6">
+                      {ong.name}
+                    </h2>
+                    <p className="text-center text-white opacity-80 mb-6 text-lg">
+                      {ong.description}
                     </p>
-                    <a href="/login"
-                        className="text-decoration-none "
-                    >
-                        <FancyButton text="Înregistrează-te" />
-                    </a>
+                    {ong.website && (
+                      <a
+                        href={ong.website}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="block text-center text-white bg-blue-700 hover:bg-blue-800 rounded-full py-2 px-6 mt-4 transition-all duration-300 ease-in-out"
+                      >
+                        Vizitează site-ul
+                      </a>
+                    )}
+                  </div>
                 </div>
-            </main>
-            <Footer />
+              </Link>
+            ))
+          ) : (
+            <p className="text-center text-gray-500 justify-center flex items-center justify-center text-xl font-semibold">
+              <span className="mr-2">Încărcăm datele...</span>
+              <svg
+                className="animate-spin h-5 w-5 text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                  fill="none"
+                />
+                <path
+                  d="M4 12a8 8 0 1 1 16 0A8 8 0 0 1 4 12z"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+              </svg>
+            </p>
+          )}
         </div>
-    );
+        <div className="text-center">
+          <p className="text-lg text-center mb-8">
+            Ești un ONG care dorește să devi parte din echipa de parteneri
+            alătură-te.
+          </p>
+          <a href="/login" className="text-decoration-none">
+            <FancyButton text="Înregistrează-te" />
+          </a>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
-
--- src/pages/ong/[id].tsx
+++ src/pages/ong/[id].tsx
@@ -5,95 +5,131 @@ import Footer from "@/components/footer";
 import Sidebar from "./sidebar";
 import { info } from "console";
 import FancyButton from "@/components/fancyButton";
+import { User } from "@/lib/models/user";
 
 interface Shelter {
-    name: string;
-    description: string;
-    image?: string;
-    website?: string;
+  _id: string;
+  name: string;
+  description: string;
+  image?: string;
+  website?: string;
+}
+
+interface Pet {
+  nume: string;
+  specie: string;
+  rasa: string;
+  varsta: { ani: number; luni: number };
+  gen: string;
+  greutate: number;
+  culoare: string;
+  dataSalvarii: string;
+  stareDeSanatate: string;
+  sterilizat: string;
+  disponibil: string;
+  descriere: string;
+  addedBy: string;
+  image: string;
 }
 
 export default function OngDetails() {
-    const router = useRouter();
-    const { id } = router.query;
+  const router = useRouter();
+  const { id } = router.query;
 
-    const [ongs, setOngs] = useState<Shelter[]>([]);
-    const [selectedShelter, setSelectedShelter] = useState<Shelter | null>(null);
-    const [selectedFunctionality, setSelectedFunctionality] = useState<string | null>(null);
+  const [ongs, setOngs] = useState<Shelter[]>([]);
+  const [selectedShelter, setSelectedShelter] = useState<Shelter | null>(null);
+  const [selectedFunctionality, setSelectedFunctionality] = useState<
+    string | null
+  >(null);
+  const [petsForThisShelter, setPetsForThisShelter] = useState<Pet[]>([]);
+  const [pets, setPets] = useState<Pet[]>([]);
 
-    useEffect(() => {
-        fetch("/api/getShelters")
-            .then((response) => response.json()) 
-            .then((data: Shelter[]) => setOngs(data))
-            .catch((error) => console.error("Error fetching NGOs:", error));
-    }, []);
+  useEffect(() => {
+    fetch("/api/getShelters")
+      .then((response) => response.json())
+      .then((data: Shelter[]) => setOngs(data))
+      .catch((error) => console.error("Error fetching NGOs:", error));
+  }, []);
 
-    useEffect(() => {
-        if (id && ongs.length > 0) {
-            const foundShelter = ongs.find((element) => element.name === id);
-            setSelectedShelter(foundShelter || null);
-        }
-    }, [id, ongs]);
-
-    if (!selectedShelter) {
-        return (
-            <div className="min-h-screen flex flex-col items-center justify-center">
-                <p className="text-xl text-gray-500">Încărcăm datele...</p>
-            </div>
-        );
+  useEffect(() => {
+    if (id && ongs.length > 0) {
+      const foundShelter = ongs.find((element) => element._id === id);
+      setSelectedShelter(foundShelter || null);
     }
+  }, [id, ongs]);
 
+  useEffect(() => {
+    fetch("/api/getPet")
+      .then((response) => response.json())
+      .then((data: Pet[]) => setPets(data))
+      .catch((error) => console.error("Error fetching pets:", error));
+  }, []);
+
+  useEffect(() => {
+    // const findPet = pets.find((element) => element.addedBy === selectedShelter);
+    // setPetsForThisShelter(findPet? [findPet]: []);
+  }, [petsForThisShelter]);
+
+  if (!selectedShelter) {
     return (
-        <main className="flex-grow gap-3">
-            <Header />
-            <div className="flex flex-col md:flex-row">
-                {/* Sidebar with shelter details */}
-                <Sidebar 
-                    name={selectedShelter.name} 
-                    description={selectedShelter.description} 
-                    logo={selectedShelter.image || "/default-logo.png"} 
-                    website={selectedShelter.website || "#"}
-                    onFunctionalitySelect={setSelectedFunctionality} // Pass the state updater
-                />
-
-                {/* Main Content */}
-                <div className="flex-1 p-8">
-                    <h1 className="text-4xl font-bold">{selectedShelter.name}</h1>
-                    <p className="text-lg mt-4">{selectedShelter.description}</p>
-
-                    {/* Show content based on selected functionality */}
-                    {selectedFunctionality === "animals" && (
-                        <div className="mt-6 p-4 bg-gray-100 rounded-lg">
-                            <h2 className="text-2xl font-semibold">Animals Information</h2>
-                            <p>Empty for now</p>
-                        </div>
-                    )}
-
-                    {selectedFunctionality === "evenimente" && (
-                        <div className="mt-6 p-4 bg-gray-100 rounded-lg">
-                            <h2 className="text-2xl font-semibold">Evenimente Information</h2>
-                            <p>Empty for now</p>
-                        </div>
-                    )}
-
-                    {selectedFunctionality === null && (
-                        <div className="flex flex-col items-center justify-center h-[60vh] bg-gray-100 rounded-lg shadow-lg p-6 text-center">
-                            <h2 className="text-3xl font-bold text-gray-800">Bine ai venit la {selectedShelter.name}!</h2>
-                            <p className="mt-4 text-lg text-gray-700 max-w-md">
-                                Aceasta este pagina de prezentare, iar echipa noastră face tot posibilul să ajute comunitatea.
-                                Dar tu cum poți contribui?
-                            </p>
-                            <div className="mt-6">
-                                <a href="/donate">
-                                    <FancyButton text="Donează" />
-                                </a>
-                            </div>
-                        </div>
-                    )}
-
-                </div>
-            </div>
-            <Footer />
-        </main>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <p className="text-xl text-gray-500">Încărcăm datele...</p>
+      </div>
     );
+  }
+
+  return (
+    <main className="flex-grow gap-3">
+      <Header />
+      <div className="flex flex-col md:flex-row">
+        {/* Sidebar with shelter details */}
+        <Sidebar
+          name={selectedShelter.name}
+          description={selectedShelter.description}
+          logo={selectedShelter.image || "/default-logo.png"}
+          website={selectedShelter.website || "#"}
+          onFunctionalitySelect={setSelectedFunctionality} // Pass the state updater
+        />
+
+        {/* Main Content */}
+        <div className="flex-1 p-8">
+          <h1 className="text-4xl font-bold">{selectedShelter.name}</h1>
+          <p className="text-lg mt-4">{selectedShelter.description}</p>
+
+          {/* Show content based on selected functionality */}
+          {selectedFunctionality === "animals" && (
+            <div className="mt-6 p-4 bg-gray-100 rounded-lg">
+              <h2 className="text-2xl font-semibold">Animals Information</h2>
+              <p>Empty for now</p>
+            </div>
+          )}
+
+          {selectedFunctionality === "evenimente" && (
+            <div className="mt-6 p-4 bg-gray-100 rounded-lg">
+              <h2 className="text-2xl font-semibold">Evenimente Information</h2>
+              <p>Empty for now</p>
+            </div>
+          )}
+
+          {selectedFunctionality === null && (
+            <div className="flex flex-col items-center justify-center h-[60vh] bg-gray-100 rounded-lg shadow-lg p-6 text-center">
+              <h2 className="text-3xl font-bold text-gray-800">
+                Bine ai venit la {selectedShelter.name}!
+              </h2>
+              <p className="mt-4 text-lg text-gray-700 max-w-md">
+                Aceasta este pagina de prezentare, iar echipa noastră face tot
+                posibilul să ajute comunitatea. Dar tu cum poți contribui?
+              </p>
+              <div className="mt-6">
+                <a href="/donate">
+                  <FancyButton text="Donează" />
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
 }
